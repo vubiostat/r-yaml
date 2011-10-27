e87bf9edf807c822da13f8f4f0d6891a0b06e688
Jeremy Stephens <viking@pillageandplunder.net>
2011-10-26 19:45:56 -0500

    Add anchor/alias support, hooray!

--- src/r-ext.c
+++ src/r-ext.c
@@ -3,53 +3,90 @@
 #include "R.h"
 #include "Rdefines.h"
 #include "R_ext/Rdynload.h"
-#include <yaml.h>
+#include "yaml.h"
+#include "yaml_private.h"
 
 static SEXP R_KeysSymbol = NULL;
 
 typedef struct {
+  int refcount;
   SEXP obj;
-  PROTECT_INDEX ipx;  /* in case we need coercion */
+
+  /* This is for tracking whether or not this object has a parent.
+   * If there is no parent, that means this object should be UNPROTECT'd
+   * when assigned to a parent SEXP object */
+  int orphan;
+} s_prot_object;
+
+typedef struct {
+  s_prot_object *obj;
   yaml_event_type_t type;
   void *prev;
 } s_stack_entry;
 
 typedef struct {
-  SEXP obj;
+  yaml_char_t *name;
+  s_prot_object *obj;
   void *prev;
 } s_alias_entry;
 
+static s_prot_object *
+new_prot_object(obj)
+  SEXP obj;
+{
+  s_prot_object *result;
+
+  result = (s_prot_object *)malloc(sizeof(s_prot_object));
+  result->refcount = 0;
+  result->obj = obj;
+  result->orphan = 1;
+
+  return result;
+}
+
 static void
-stack_push(stack, type, obj, ipx)
+prune_prot_object(obj)
+  s_prot_object *obj;
+{
+  if (obj->orphan == 1) {
+    /* obj is now part of list and is therefore protected */
+    UNPROTECT_PTR(obj->obj);
+    obj->orphan = 0;
+  }
+
+  if (obj->refcount == 0) {
+    /* Don't need this object wrapper anymore */
+    free(obj);
+  }
+}
+
+static void
+stack_push(stack, type, obj)
   s_stack_entry **stack;
   yaml_event_type_t type;
-  SEXP obj;
-  PROTECT_INDEX ipx;
+  s_prot_object *obj;
 {
   s_stack_entry *result;
 
   result = (s_stack_entry *)malloc(sizeof(s_stack_entry));
   result->type = type;
   result->obj = obj;
-  result->ipx = ipx;
+  obj->refcount++;
   result->prev = *stack;
 
   *stack = result;
 }
 
 static void
-stack_pop(stack, obj, ipx)
+stack_pop(stack, obj)
   s_stack_entry **stack;
-  SEXP *obj;
-  PROTECT_INDEX *ipx;
+  s_prot_object **obj;
 {
   s_stack_entry *result, *top;
 
   top = *stack;
   *obj = top->obj;
-  if (ipx != NULL) {
-    *ipx = top->ipx;
-  }
+  top->obj->refcount--;
   result = (s_stack_entry *)top->prev;
   free(top);
 
@@ -57,38 +94,49 @@ stack_pop(stack, obj, ipx)
 }
 
 static void
-handle_start_event(event, stack, aliases)
+handle_alias(event, stack, aliases)
   yaml_event_t *event;
   s_stack_entry **stack;
-  s_alias_entry **aliases;
+  s_alias_entry *aliases;
 {
-  stack_push(stack, event->type, NULL, -1);
+  s_alias_entry *alias = aliases;
+  while (alias) {
+    if (strcmp((char *)alias->name, event->data.alias.anchor) == 0) {
+      stack_push(stack, event->type, alias->obj);
+      break;
+    }
+  }
 }
 
 static void
-handle_scalar(event, stack, aliases)
+handle_start_event(event, stack)
   yaml_event_t *event;
   s_stack_entry **stack;
-  s_alias_entry **aliases;
+{
+  stack_push(stack, event->type, new_prot_object(NULL, -1));
+}
+
+static void
+handle_scalar(event, stack)
+  yaml_event_t *event;
+  s_stack_entry **stack;
 {
   SEXP obj;
-  PROTECT_INDEX ipx;
 
-  PROTECT_WITH_INDEX(obj = NEW_STRING(1), &ipx);
+  PROTECT(obj = NEW_STRING(1));
   SET_STRING_ELT(obj, 0, mkChar((char *)event->data.scalar.value));
-  stack_push(stack, event->type, obj, ipx);
+  stack_push(stack, event->type, new_prot_object(obj));
 }
 
 static void
-handle_sequence(event, stack, aliases)
+handle_sequence(event, stack)
   yaml_event_t *event;
   s_stack_entry **stack;
-  s_alias_entry **aliases;
 {
   s_stack_entry *stack_ptr;
+  s_prot_object *obj;
   int count, i;
-  SEXP list, obj;
-  PROTECT_INDEX ipx;
+  SEXP list;
 
   /* Find out how many elements there are */
   stack_ptr = *stack;
@@ -100,31 +148,28 @@ handle_sequence(event, stack, aliases)
 
   /* Initialize list */
   list = allocVector(VECSXP, count);
-  PROTECT_WITH_INDEX(list, &ipx);
+  PROTECT(list);
 
   /* Populate the list, popping items off the stack as we go */
   for (i = count - 1; i >= 0; i--) {
-    stack_pop(stack, &obj, NULL);
-    SET_VECTOR_ELT(list, i, obj);
-
-    /* obj is now part of list and is therefore protected */
-    UNPROTECT_PTR(obj);
+    stack_pop(stack, &obj);
+    SET_VECTOR_ELT(list, i, obj->obj);
+    prune_prot_object(obj);
   }
-  (*stack)->obj = list;
-  (*stack)->ipx = ipx;
+  (*stack)->obj->obj = list;
 }
 
 static void
-handle_map(event, stack, aliases, coerce)
+handle_map(event, stack, coerce)
   yaml_event_t *event;
   s_stack_entry **stack;
-  s_alias_entry **aliases;
   int coerce;
 {
+  s_prot_object *obj;
   s_stack_entry *stack_ptr;
-  int count, i;
-  SEXP list, keys, obj, key;
-  PROTECT_INDEX ipx, obj_ipx;
+  int count, i, orphan_key;
+  SEXP list, keys, key, key_str;
+  PROTECT_INDEX ipx;
 
   /* Find out how many elements there are */
   stack_ptr = *stack;
@@ -148,37 +193,45 @@ handle_map(event, stack, aliases, coerce)
 
   /* Populate the list, popping items off the stack as we go */
   for (i = count - 1; i >= 0; i--) {
-    stack_pop(stack, &obj, &obj_ipx);
+    stack_pop(stack, &obj);
 
     if (i % 2 == 1) {
       /* map value */
-      SET_VECTOR_ELT(list, i / 2, obj);
+      SET_VECTOR_ELT(list, i / 2, obj->obj);
     }
     else {
       /* map key */
       /* TODO: handle duplicate keys */
       if (coerce) {
-        REPROTECT(obj = AS_CHARACTER(obj), obj_ipx);
-        switch (LENGTH(obj)) {
+        key = AS_CHARACTER(obj->obj);
+        orphan_key = (key != obj->obj);
+        if (orphan_key) {
+          /* This key has been coerced into a character, and is a new object. */
+          PROTECT(key);
+        }
+
+        switch (LENGTH(key)) {
           case 0:
             warning("Empty character vector used as a list name");
-            key = mkChar("");
+            key_str = mkChar("");
             break;
           default:
             warning("Character vector of length greater than 1 used as a list name");
           case 1:
-            key = STRING_ELT(obj, 0);
+            key_str = STRING_ELT(key, 0);
             break;
         }
-        SET_STRING_ELT(keys, i / 2, key);
+        SET_STRING_ELT(keys, i / 2, key_str);
+
+        if (orphan_key) {
+          UNPROTECT(1);
+        }
       }
       else {
-        SET_VECTOR_ELT(keys, i / 2, obj);
+        SET_VECTOR_ELT(keys, i / 2, obj->obj);
       }
     }
-
-    /* obj is now part of values or keys and is therefore protected */
-    UNPROTECT_PTR(obj);
+    prune_prot_object(obj);
   }
 
   if (coerce) {
@@ -189,13 +242,30 @@ handle_map(event, stack, aliases, coerce)
   }
   UNPROTECT_PTR(keys);
 
-  stack_ptr->obj = list;
-  stack_ptr->ipx = ipx;
+  stack_ptr->obj->obj = list;
   *stack = stack_ptr;
 }
 
+static void
+possibly_record_alias(anchor, aliases, obj)
+  yaml_char_t *anchor;
+  s_alias_entry **aliases;
+  s_prot_object *obj;
+{
+  s_alias_entry *alias;
+
+  if (anchor != NULL) {
+    alias = (s_alias_entry *)malloc(sizeof(s_alias_entry));
+    alias->name = yaml_strdup(anchor);
+    alias->obj = obj;
+    obj->refcount++;
+    alias->prev = *aliases;
+    *aliases = alias;
+  }
+}
+
 static int
-R_is_named_list( obj )
+R_is_named_list(obj)
   SEXP obj;
 {
   SEXP names;
@@ -212,6 +282,7 @@ load_yaml_str(s_str, s_use_named, s_handlers)
   SEXP s_use_named;
   SEXP s_handlers;
 {
+  s_prot_object *obj;
   SEXP retval, R_hndlr, cmd, names;
   yaml_parser_t parser;
   yaml_event_t event;
@@ -220,7 +291,7 @@ load_yaml_str(s_str, s_use_named, s_handlers)
   long len;
   int use_named, i, done = 0;
   s_stack_entry *stack = NULL;
-  s_alias_entry *aliases = NULL;
+  s_alias_entry *aliases = NULL, *alias;
 
   if (!isString(s_str) || length(s_str) != 1) {
     error("first argument must be a character vector of length 1");
@@ -250,37 +321,42 @@ load_yaml_str(s_str, s_use_named, s_handlers)
       switch (event.type) {
         case YAML_ALIAS_EVENT:
           printf("ALIAS: %s\n", event.data.alias.anchor);
+          handle_alias(&event, &stack, aliases);
           break;
 
         case YAML_SCALAR_EVENT:
           printf("SCALAR: %s (%s)\n", event.data.scalar.value, event.data.scalar.tag);
-          handle_scalar(&event, &stack, &aliases);
+          handle_scalar(&event, &stack);
+          possibly_record_alias(event.data.scalar.anchor, &aliases, stack->obj);
           break;
 
         case YAML_SEQUENCE_START_EVENT:
           printf("SEQUENCE START: (%s)\n", event.data.sequence_start.tag);
-          handle_start_event(&event, &stack, &aliases);
+          handle_start_event(&event, &stack);
+          possibly_record_alias(event.data.sequence_start.anchor, &aliases, stack->obj);
           break;
 
         case YAML_SEQUENCE_END_EVENT:
           printf("SEQUENCE END\n");
-          handle_sequence(&event, &stack, &aliases);
+          handle_sequence(&event, &stack);
           break;
 
         case YAML_MAPPING_START_EVENT:
           printf("MAPPING START: (%s)\n", event.data.mapping_start.tag);
-          handle_start_event(&event, &stack, &aliases);
+          handle_start_event(&event, &stack);
+          possibly_record_alias(event.data.mapping_start.anchor, &aliases, stack->obj);
           break;
 
         case YAML_MAPPING_END_EVENT:
           printf("MAPPING END\n");
-          handle_map(&event, &stack, &aliases, use_named);
+          handle_map(&event, &stack, use_named);
           break;
 
         case YAML_STREAM_END_EVENT:
           if (stack != NULL) {
-            stack_pop(&stack, &retval, NULL);
-            UNPROTECT_PTR(retval);
+            stack_pop(&stack, &obj);
+            retval = obj->obj;
+            prune_prot_object(obj);
           }
           else {
             retval = R_NilValue;
@@ -349,6 +425,15 @@ load_yaml_str(s_str, s_use_named, s_handlers)
     }
 
     yaml_event_delete(&event);
+  }
+
+  /* Clean up aliases */
+  while (aliases != NULL) {
+    alias = aliases;
+    aliases = aliases->prev;
+    free(alias->name);
+    free(alias->obj);
+    free(alias);
   }
 
   yaml_parser_delete(&parser);

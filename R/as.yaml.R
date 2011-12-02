c3653db1375f66ef812236d006f76be540402c09
Jeremy Stephens <viking@pillageandplunder.net>
2011-12-02 15:51:50 -0600

    as.yaml documentation fixes

--- R/as.yaml.R
+++ R/as.yaml.R
@@ -1,5 +1,5 @@
 `as.yaml` <-
-function(x, line.sep = c('\n', '\r\n', '\r'), indent = 2, omap = FALSE, column.major = TRUE, ...) {
+function(x, line.sep = c('\n', '\r\n', '\r'), indent = 2, omap = FALSE, column.major = TRUE) {
   line.sep <- match.arg(line.sep)
   .Call("as.yaml", x, line.sep, indent, omap, column.major, PACKAGE="yaml")
 }

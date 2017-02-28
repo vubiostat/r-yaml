95df3986f75aa3afa7b36bb6c475f6e12fb63bd2
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-02-28 14:19:36 -0600

    Try to guess error.label when using yaml.load_file

--- pkg/R/yaml.load_file.R
+++ pkg/R/yaml.load_file.R
@@ -1,4 +1,17 @@
 `yaml.load_file` <-
-function(input, ...) {
-  yaml.load(paste(readLines(input, encoding = 'UTF-8'), collapse="\n"), ...)
+function(input, error.label, ...) {
+  if (missing(error.label)) {
+    if (inherits(input, "connection")) {
+      # try to guess filename
+      s <- try(summary(input), silent = TRUE)
+      if (!inherits(s, "try-error") && is.list(s) && "description" %in% names(s)) {
+        error.label <- s$description
+      }
+    }
+    else if (is.character(input) && nzchar(input[1])) {
+      error.label <- input[1]
+    }
+  }
+  yaml.load(paste(readLines(input, encoding = 'UTF-8'), collapse="\n"),
+            error.label = error.label, ...)
 }

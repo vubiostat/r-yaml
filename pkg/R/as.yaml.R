c77ddb3a11f9a19f8b916a328dfb06358ef360aa
Yihui Xie <xie@yihui.name>
2017-08-18 11:48:36 -0500

    Mark the encoding of as.yaml() output as UTF-8

--- pkg/R/as.yaml.R
+++ pkg/R/as.yaml.R
@@ -1,5 +1,7 @@
 `as.yaml` <-
 function(x, line.sep = c('\n', '\r\n', '\r'), indent = 2, omap = FALSE, column.major = TRUE, unicode = TRUE, precision = getOption('digits'), indent.mapping.sequence = FALSE) {
   line.sep <- match.arg(line.sep)
-  .Call("serialize_to_yaml", x, line.sep, indent, omap, column.major, unicode, precision, indent.mapping.sequence, PACKAGE="yaml")
+  res <- .Call("serialize_to_yaml", x, line.sep, indent, omap, column.major, unicode, precision, indent.mapping.sequence, PACKAGE="yaml")
+  Encoding(res) <- "UTF-8"
+  res
 }

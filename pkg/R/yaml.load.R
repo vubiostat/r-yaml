5f6a46d34df5c8da0dc1819b1b03091f9667aa44
Yihui Xie <xie@yihui.name>
2017-08-18 11:52:34 -0500

    Let yaml.load() accept a character vector of length > 1

--- pkg/R/yaml.load.R
+++ pkg/R/yaml.load.R
@@ -1,4 +1,5 @@
 `yaml.load` <-
 function(string, as.named.list = TRUE, handlers = NULL, error.label = NULL) {
-  .Call("unserialize_from_yaml", enc2utf8(string), as.named.list, handlers, error.label, PACKAGE="yaml")
+  string <- enc2utf8(paste(string, collapse = "\n"))
+  .Call("unserialize_from_yaml", string, as.named.list, handlers, error.label, PACKAGE="yaml")
 }
